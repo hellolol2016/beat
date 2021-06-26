b9ba818c1dc69ac3158751c95085b9538a2c88b8
hellolol2016 <dennisthederp812@gmail.com>
2021-06-26 14:45:39 -0700

    added animation when pressing keyboard

--- scripts/script.js
+++ scripts/script.js
@@ -4,17 +4,18 @@ function playSound(k){
     const key = document.querySelector(`div[data-key="${k.keyCode}"]`);
     if(!audio) return;
 
-    key.classList.add("hvr-glow");
+    key.classList.add("playing");
     audio.currentTime = 0;
     audio.play();
 }
 
 function removeTransition(e){
+    console.log("remove");
     if(e.propertyName !== 'transform')return;
-    e.target.classList.remove('hvr-glow');
+    e.target.classList.remove('playing');
 }
 
-const keys = document.querySelectorAll(".key");
+const keys = Array.from(document.querySelectorAll(".key"));
 keys.forEach(key=>key.addEventListener("transitionend",removeTransition))// untransitions
 window.addEventListener("keydown", playSound);
 
